b31141d9aa91870dabdb8a63ab5fbdde69d84548
heng1025 <472263356@qq.com>
2020-12-22 00:43:26 +0800

    restore config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default {
   esbuildTarget: "es2015",
   proxy: {
     "/api": {
-      target: "http://ai.chenggang.win/api",
+      target: "http://0.0.0.0:8080/api",
       changeOrigin: true,
       rewrite: (path) => path.replace(/^\/api/, ""),
     },
