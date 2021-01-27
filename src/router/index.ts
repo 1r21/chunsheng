3eb3bad398a8a5d3421c2a08914c8b372cf63a19
iron <472263356@qq.com>
2021-01-27 14:22:13 +0000

    add env

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,15 @@ import Home from "/@/components/Home.vue";
 import Detail from "/@/components/Detail.vue";
 import NotFound from "/@/components/NotFound.vue";
 
+const { BASE_URL } = import.meta.env;
+
 const routes = [
   { path: "/", component: Home },
   { path: "/detail/:id", component: Detail },
-  { path: '/:pathMatch(.*)*', component: NotFound },
+  { path: "/:pathMatch(.*)*", component: NotFound },
 ];
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(BASE_URL),
   routes,
 });
