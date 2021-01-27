65194be5325c4d1c451059bf17ea3c00c9b95101
iron <472263356@qq.com>
2021-01-27 12:59:33 +0000

    upgrade vite 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import path from "path";
+import vue from "@vitejs/plugin-vue";
 
 export default {
+  plugins: [vue()],
   alias: {
-    "/@/": path.resolve(__dirname, "src"),
+    "/@": path.resolve(__dirname, "src"),
   },
   // support optional chain [build mode]
   esbuildTarget: "es2015",
   proxy: {
-    "/api": {
-      target: "http://0.0.0.0:8080/api",
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ""),
-    },
+    "/api": "http://0.0.0.0:8080/api",
   },
 };
